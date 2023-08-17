11a3a48b403a92d8168ba8062e5df8724f65c528
Jeglerjeg <jegler@heckin.how>
2023-08-17 20:30:10 +0200

    Use simple dictionary for full text search

---
+++ migrations/2023-08-17-182355_use_simple_dictionary_tsvector/down.sql
@@ -0,0 +1,6 @@
+ALTER TABLE summary_messages DROP COLUMN ts;
+
+ALTER TABLE summary_messages ADD COLUMN ts tsvector NOT NULL
+    GENERATED ALWAYS AS (to_tsvector('english', content)) STORED;
+
+CREATE INDEX textsearch_content_index ON summary_messages USING GIN (ts)
